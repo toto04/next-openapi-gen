e9a73221c8826b51145c9c3db5bf21b8b5faf1cf
Mariusz Winnik <tazo90@gmail.com>
2024-11-12 21:44:37 +0100

    fix: use --legacy-peer-deps because swagger-ui-react does not support react19 now

--- src/components/swagger.ts
+++ src/components/swagger.ts
@@ -1,4 +1,8 @@
-export const swaggerDeps = ["swagger-ui", "swagger-ui-react"];
+export const swaggerDeps = [
+  "swagger-ui",
+  "swagger-ui-react",
+  "--legacy-peer-deps", // @temp: swagger-ui-react does not support React 19 now.
+];
 
 export function SwaggerUI(outputFile: string) {
   return `
